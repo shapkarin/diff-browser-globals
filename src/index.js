d95dc4d9752a8ec7469b82e8cad674ff7eb41848
Yury Shapkarin <yury@shapkarin.me>
2020-07-06 00:09:08 +0300

    add additional globals

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { browser } from 'globals';
 
+import toAddLaterWithPR from './toAddLaterWithPR';
 
 export default function(){
   try {
-    const defaultKeys = new Set(Object.keys(browser));
+    const defaultKeys = new Set([...Object.keys(browser), ...toAddLaterWithPR]);
     const yours = new Set(Object.keys(window));
     
     return new Set([...yours].filter(key => !defaultKeys.has(key)));
