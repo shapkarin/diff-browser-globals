c36b28648d592862dd3636f923478ee27b52d047
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-07-07 23:14:11 +1000

    Update src/index.js
    Co-authored-by: Yury Shapkarin <yury@shapkarin.me>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import toAddLaterWithPR from './toAddLaterWithPR';
 export default function(){
   const defaultKeys = Object.keys(browser)
     .concat(toAddLaterWithPR)
-    .reduce((acc, n) => { ...acc, [n]: true }, {});
+    .reduce((acc, n) => ({ ...acc, [n]: true }), {});
 
   return Object.keys(window).filter(key => !defaultKeys[key]);
 };
