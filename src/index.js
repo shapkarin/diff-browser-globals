80812e3673ae2afcc48b36e8d0512de5fde7651d
Shapkarin <yury@shapkarin.me>
2024-06-18 21:53:53 +0300

    refactor: remove draft due globals package automatically generate detected list
    https://github.com/sindresorhus/globals/issues/164#issuecomment-2081338803

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import { browser } from 'globals';
 
-import toAddLaterWithPR from './toAddLaterWithPR';
-
 export default function(){
   const defaultKeys = Object.keys(browser)
-    .concat(toAddLaterWithPR)
     .reduce((acc, n) => ({ ...acc, [n]: true }), {});
 
   return Object.keys(window).filter(key => !defaultKeys[key]);
